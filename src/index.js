0c9244b0cbd7f924423cb7aefafdc517f0c8cfa2
BaeKILB <backgroundp7@gmail.com>
2023-12-02 01:43:05 +0900

    스크롤시 네비바 동작 구현

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 let navList = document.querySelectorAll(".nav-link");
+let menuList = [];
+
+for (let navItem of navList) {
+  menuList.push(
+    document.querySelector(
+      "#" + navItem.attributes["data-nav-section"].nodeValue
+    )
+  );
+}
 
 function onNavClickHandler(e) {
   for (let navItem of navList) {
@@ -14,3 +23,23 @@ function onNavClickHandler(e) {
 for (let navItem of navList) {
   navItem.addEventListener("click", onNavClickHandler);
 }
+
+// 스크롤시 메뉴바 동작
+let observer = new IntersectionObserver(
+  (items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        for (let navItem of navList) {
+          navItem.classList.remove("active");
+        }
+        navList[item.target.attributes["tabindex"].nodeValue].classList.add(
+          "active"
+        );
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+menuList.forEach((menuItem) => observer.observe(menuItem));
